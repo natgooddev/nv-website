895eea32688f8588cb968dffc957d19c7ca4108f
natgooddev <gooddev639@gmail.com>
2019-01-31 13:29:29 +0700

    less4

--- server/src/app.js
+++ server/src/app.js
@@ -19,6 +19,33 @@ app.post('/hello', function (req, res) {
   res.send('OK  you post - ' + req.body.name)
 })
 
+/* RESFUL Api for users management */
+// create user
+app.post('/user', function (req, res) {
+  res.send('สร้างผู้ใช้')
+})
+
+// edit user, suspend, active
+app.put('/user/:userId', function (req, res) {
+  res.send('แก้ไขผู้ใช้')
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+  res.send('ลบผู้ใช้งาน')
+})
+
+// get user by id
+app.get('/user/:userId', function (req, res) {
+  res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+// get all user
+app.get('/users', function (req, res) {
+  res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
+})
+
+
 
 let port = 8080
 
