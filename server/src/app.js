3b0bf2c9f45aad521b418a68e17badc5dd18f608
natgooddev <gooddev639@gmail.com>
2019-01-31 10:34:59 +0700

    less1-3

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/status', function (req, res ){
+  res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req,res) {
+  console.log('hello - ' + req.params.person)
+  res.send('sey hello with ' + req.params.person)
+})
+
+app.post('/hello', function (req, res) {
+  res.send('OK  you post - ' + req.body.name)
+})
+
+
+let port = 8080
+
+app.listen(port, function () {
+  console.log('server running on ' + port)
+})
